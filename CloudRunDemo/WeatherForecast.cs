1c866b59df890fe72ea14a1f8d5f287b7ffea86f
Zubayer Bin Ayub <zubayer01913@gmail.com>
2023-12-18 19:51:08 +0600

    remove class code

--- CloudRunDemo/WeatherForecast.cs
+++ CloudRunDemo/WeatherForecast.cs
@@ -12,14 +12,4 @@ namespace CloudRunDemo
     }
 
 
-    public class WeatherForecastTest
-    {
-        public DateTime Date { get; set; }
-
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
-    }
 }
